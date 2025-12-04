f28bbf86567204b6965852cb8edc0c302a30c494
Erick <erickbs2006@gmail.com>
2025-12-04 11:30:02 -0300

    adicionado footer

--- js/perfis.js
+++ js/perfis.js
@@ -1,8 +1,6 @@
 const perfis = [
     { nome: "Erick", img: "assets/perfil.png" },
     { nome: "Elton", img: "assets/perfil.png" },
-    { nome: "Derick", img: "assets/perfil.png" },
-    { nome: "Hian", img: "assets/perfil.png" },
-    { nome: "Joab", img: "assets/perfil.png" },
-    { nome: "Wanderson", img: "assets/perfil.png" }
+    { nome: "Hian", img: "assets/perfil.png" }
+
 ];
